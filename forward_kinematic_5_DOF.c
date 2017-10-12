2d6cd121fb576644667ab8baffa9dadf7682a99d
Aditya Pandey <addherbs@gmail.com>
2017-10-11 21:01:37 -0500

    initialized rotation matrix

--- forward_kinematic_5_DOF.c
+++ forward_kinematic_5_DOF.c
@@ -45,6 +45,51 @@ void calculate_disp_matrix(char axis, double displacement, double matDisp[N][N])
 	default: 
 		printf("this is the default case\n");
 	}
+
+}
+
+
+void calculate_rot_matrix(char axis, double thetaValue, double matRotate[N][N]) {
+
+	double s;
+	double c;
+	double minusS;
+	
+	double val = M_PI / 180.0;
+	c = cos(thetaValue*val);
+	s = sin(thetaValue*val);
+	minusS = (-1) * s;
+	
+	switch (axis) {
+	case 'x':
+		printf("this is the x case\n");
+		matRotate[1][1] = c;
+		matRotate[1][2] = minusS;
+		matRotate[2][1] = s;
+		matRotate[2][2] = c;
+		break;
+
+	case 'y':
+		printf("this is the y case\n");
+		matRotate[0][0] = c;
+		matRotate[0][2] = minusS;
+		matRotate[2][0] = s;
+		matRotate[2][2] = c;
+		break;
+
+	case 'z':
+		printf("this is the z case\n");
+		matRotate[0][0] = c;
+		matRotate[0][1] = minusS;
+		matRotate[1][0] = s;
+		matRotate[1][1] = c;
+		break;
+
+	default:
+		printf("this is the default case\n");
+	}
+
+
 }
 
 

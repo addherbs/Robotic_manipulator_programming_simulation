6ad8391ce8b4b105b3f0ce9a143c6261dfb0e621
Aditya Pandey <addherbs@gmail.com>
2017-10-11 21:00:41 -0500

    Added matrix multiplication method

---
+++ forward_kinematic_5_DOF.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+
+#define N 4
+#ifndef M_PI
+#define M_PI 3.1415927
+#endif
+
+
+
+void multiply(int mat1[][N], int mat2[][N], int res[][N])
+{
+    int i, j, k;
+    for (i = 0; i < N; i++)
+    {
+        for (j = 0; j < N; j++)
+        {
+            res[i][j] = 0;
+            for (k = 0; k < N; k++)
+                res[i][j] += mat1[i][k]*mat2[k][j];
+        }
+    }
+}
+
+
+
